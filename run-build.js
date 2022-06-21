feb4008f17b1c1ec726a1bd12a02191b1c57bf1e
Jason Yee <446031+jwsy@users.noreply.github.com>
2022-06-21 09:23:40 +0900

    Update run-build.js

--- run-build.js
+++ run-build.js
@@ -39,7 +39,7 @@ function buildGame() {
 			sourcemap: true,
 			target: "es6",
 			keepNames: true,
-			// logLevel: "silent",
+			logLevel: "debug",
 			entryPoints: ["code/main.js"],
 			outfile: "dist/game.js",
 		});
